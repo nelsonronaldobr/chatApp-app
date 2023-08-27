ae645baa96be1b1fd0efbd4065daa8dfd6a86e97
NelsonBarreraRivas <nelsonbr862@gmail.com>
2023-08-27 18:39:20 -0500

    hidden alert copyclipboard

--- src/helpers/copyClipboard.ts
+++ src/helpers/copyClipboard.ts
@@ -2,12 +2,7 @@ export const copyClipboard = async (messageContent: string) => {
     try {
         // Copiar el contenido al portapapeles
         await navigator.clipboard.writeText(messageContent);
-
-        // Mostrar una retroalimentación (opcional)
-        alert('Mensaje copiado al portapapeles');
     } catch (error) {
         console.error('Error al copiar al portapapeles:', error);
-        // Mostrar una retroalimentación en caso de error (opcional)
-        alert('Error al copiar al portapapeles');
     }
 };
