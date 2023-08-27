e7f91f3f17321521a6e42988bf0eb1aab1262ae3
NelsonBarreraRivas <nelsonbr862@gmail.com>
2023-08-27 01:42:53 -0500

    :)

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { ChatApp } from './ChatApp';
 import { Provider } from 'react-redux';
 import { store } from '@/store';
 import './globals.css';
-
+/* :) */
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     //<React.StrictMode>
     <Provider store={store}>
